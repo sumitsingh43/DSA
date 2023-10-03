4d73a35247c16170f7397534d621e7e0a1a7c92f
Sumit Kumar <64541041+sumitsingh43@users.noreply.github.com>
2023-10-03 19:44:14 +0530

    docs: input String
    taking string from user

---
+++ ARRAY/String/InputString.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main{
+public static String inputTaking(){
+    Scanner scn=new Scanner(System.in);
+     String str=scn.nextLine();
+     return str;
+}
+
+public static void main (String args[]){
+System.out.println(" enter your string");
+ System.out.println( inputTaking());
+}}
