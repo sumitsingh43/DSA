058bbc9f9d4efb4c8e9b80aae0716ee68d84d36d
Sumit Kumar <64541041+sumitsingh43@users.noreply.github.com>
2023-10-05 19:46:27 +0530

    arraylist day1
    arraylist

---
+++ ArrayList/Basic.java
@@ -0,0 +1,10 @@
+
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        
+        ArrayList<Integer> list=new ArrayList <>();
+        list.add(3);
+        System.out.print("this is arraylist"+ " "  + list);
+    }
+}
