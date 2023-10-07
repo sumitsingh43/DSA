83a12465ba3514080556bfc1d125e1f72191dd4b
Sumit Kumar <64541041+sumitsingh43@users.noreply.github.com>
2023-10-07 23:33:44 +0530

    docs; bubble short
    sorting algorithm

---
+++ ARRAY/BubbleShort.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void bubbleSort(int arr[]) {
+        for (int i = 0; i < arr.length; ++i) {
+            for (int j = 0; j < arr.length - 1 - i; ++j) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 9, 7, 8, 5, 6, 3, 2, 1 }; // Example input in descending order
+        bubbleSort(arr);
+        for (int i = 0; i < arr.length; ++i) {
+            System.out.print(" " + arr[i]);
+        }
+    }
+}
+// for descending order; rrun it on online complier otherwise change classname.. thank you .
+// sks
