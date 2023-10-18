a0f85bfd719224fc7ec1ff2445d8afd3e6fa9fcf
Sumit Kumar <64541041+sumitsingh43@users.noreply.github.com>
2023-10-18 18:42:28 +0530

    Update factorial.java
    factorial

--- Number/factorial.java
+++ Number/factorial.java
@@ -1,6 +1,9 @@
 import java.util.*;
 public class Main{
     public static int factorial(int arr){
+        if( arr==0){
+            return 1;
+        }
    int fact=1;
    for(int i=arr;i>=1;--i){
        fact*=i;
@@ -9,8 +12,7 @@ public class Main{
     }
     
     public static void main(String args[]){
-        int arr=5;
+        int arr=0;
        System.out.print(" " + factorial(arr));
     }
 }
-//sks
